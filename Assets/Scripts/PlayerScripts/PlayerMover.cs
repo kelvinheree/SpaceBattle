72f39a8eda570c7c97013a1057376dbc8b82d8af
vlauffer <vlauffer@gmail.com>
2017-09-18 13:36:29 -0400

    Wormhole
    orgnaized folders and created 4 directions of movement. need to add mouse tracking.

---
+++ Assets/Scripts/PlayerScripts/PlayerMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+
+	float forwardSpeed=10f;
+	float sideSpeed=5f;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horz = Input.GetAxis ("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+		Vector3 move = new Vector3 (horz*sideSpeed,0f,vert*sideSpeed);
+		rb.AddForce (move);
+	}
+}
