114e79def41623b803efec4d168e1c84794cdaaa
gavinyahna <31702834+gavinyahna@users.noreply.github.com>
2017-09-21 21:57:06 -0400

    Added Force Field
    Added in a force field that will be the game zone restriction and changed the player movement scrupt so you can look and move around

--- Assets/Scripts/PlayerScripts/PlayerMover.cs
+++ Assets/Scripts/PlayerScripts/PlayerMover.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour {
 
-	float forwardSpeed=10f;
-	float sideSpeed=5f;
-	Rigidbody rb;
+	float forwardSpeed = 10f;
+	float sideSpeed = 5f;
+    float horizontalRotateSpeed = 1.0f;
+    float verticalRotateSpeed = 1.0f;
+    Rigidbody rb;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		float horz = Input.GetAxis ("Horizontal");
+
+    void Update()
+    {
+        float h = horizontalRotateSpeed * Input.GetAxis("Mouse X");
+        float v = verticalRotateSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(-v, h, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * forwardSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(transform.forward * -forwardSpeed);
+        }
+        
+        
+        /*
+        float horz = Input.GetAxis ("Horizontal");
 		float vert = Input.GetAxis ("Vertical");
 		Vector3 move = new Vector3 (horz*sideSpeed,0f,vert*sideSpeed);
 		rb.AddForce (move);
+        */
 	}
 }
