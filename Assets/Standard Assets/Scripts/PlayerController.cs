bb113c62d9a50341346e07d5ee92b3f8b9121c8f
Kelvin <kelvinmei1996@gmail.com>
2017-09-27 23:28:55 -0400

    More Unet Movement Stuff

--- Assets/Standard Assets/Scripts/PlayerController.cs
+++ Assets/Standard Assets/Scripts/PlayerController.cs
@@ -1,15 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
     void Update()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
     }
 }
