f1fc6c0fd1cd5246ed231b7669e354427c4d0c76
Kelvin <kelvinmei1996@gmail.com>
2017-10-11 02:50:27 -0400

    Shooting setup + Unique Player IDs

--- Assets/Space/Scripts/PlayerSetup.cs
+++ Assets/Space/Scripts/PlayerSetup.cs
@@ -4,25 +4,43 @@ using UnityEngine.Networking;
 public class PlayerSetup : NetworkBehaviour {
 	[SerializeField]
 	Behaviour[] componentsToDisable;
+	[SerializeField]
+	string remoteLayerName = "RemotePlayer";
 
 	Camera mainCam;
 
 	void Start(){
 		if (!isLocalPlayer){
-			for (int i = 0; i < componentsToDisable.Length; i++){
-				componentsToDisable[i].enabled = false;
-			}
+			DisableComponents();
+			AssignRemoteLayer();
 		} else {
 			mainCam = Camera.main;
 			if (mainCam != null){
 				mainCam.gameObject.SetActive(false);
 			}
 		}
+
+		RegisterPlayer();
+	}
+
+	void RegisterPlayer(){
+		string _ID = "Player " + GetComponent<NetworkIdentity>().netId;
+		transform.name = _ID;
 	}
 
 	void OnDisable(){
 		if (mainCam != null){
 			mainCam.gameObject.SetActive(false);
 		}
+	}
+
+	void DisableComponents(){
+		for (int i = 0; i < componentsToDisable.Length; i++){
+			componentsToDisable[i].enabled = false;
+		}
+	}
+
+	void AssignRemoteLayer(){
+		gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
 	}
 }
---
+++ Assets/Space/Scripts/PlayerWeapon.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerWeapon{
+
+  public string name = "Glock";
+
+  public float damage = 10f;
+  public float range = 200f;
+
+}
