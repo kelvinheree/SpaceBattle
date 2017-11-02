ee2ae64c6deb22c1f993e99e826dbd16634f8e73
gavinyahna <31702834+gavinyahna@users.noreply.github.com>
2017-11-02 03:02:33 -0400

    added static environment and Vince's player movement script

---
+++ Assets/Scripts/Destruction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruction : MonoBehaviour {
+	public GameObject destroyedVersion;
+	// Use this for initialization
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "Player") {
+			Instantiate (destroyedVersion, transform.position, transform.rotation);
+			Destroy (gameObject);
+		}
+	}
+}
