300f2e0e4168b2b91cb2cafc75e2f6bee5a6cd11
Kelvin <kelvinmei1996@gmail.com>
2017-11-06 20:00:07 -0500

    Matchmaking

---
+++ Assets/JoinGame.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class JoinGame : MonoBehaviour {
+
+    List<GameObject> roomList = new List<GameObject>();
+
+    [SerializeField]
+    private Text status;
+
+    [SerializeField]
+    private GameObject roomListItemPrefab;
+
+    [SerializeField]
+    private Transform roomListParent;
+
+    private NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if (networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+        RefreshRoomList();
+    }
+
+
+    public void RefreshRoomList()
+    {
+        networkManager.matchMaker.ListMatches(0, 20, "", true, 0, 0, OnMatchList);
+        status.text = "Loading...";
+    }
+
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+    {
+        status.text = "";
+        if (matchList == null)
+        {
+            status.text = "Couldn't get room list";
+            return;
+        }
+        ClearRoomList();
+        foreach(MatchInfoSnapshot match in matchList)
+        {
+            GameObject _roomListItemGO = Instantiate(roomListItemPrefab);
+            _roomListItemGO.transform.SetParent(roomListParent);
+            
+        }
+    }
+
+    void ClearRoomList()
+    {
+        for (int i = 0; i <roomList.Count; i++)
+        {
+            Destroy(roomList[i]);
+        }
+
+        roomList.Clear();
+    }
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# SpaceBattleRoyale
-Cancer
